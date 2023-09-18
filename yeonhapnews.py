136297c8f389d7515822f6433568d03be4871c01
hyojung <sonhj10318@gmail.com>
2023-09-18 14:56:19 +0900

    관심도 최종(아마)

--- yeonhapnews.py
+++ yeonhapnews.py
@@ -9,7 +9,7 @@ def count_react(dic) :
   reactNum = 0
   for emo in dic :
     reactNum = reactNum + int(emo['count'])
-  return reactNum
+  return print('반응수 :', reactNum, '\n')
 
 def get_react(link) :
   result = re.search('{.+}', link.text)
@@ -17,7 +17,7 @@ def get_react(link) :
 
 for date in range(20230801,20230803) :
 
-  for page in range(3,10) :
+  for page in range(3,6) :
     params = {'mode':'LPOD', 'mid':'sec', 'oid':'001', 'date':date, 'page':page}
     yeonhap = requests.get('https://news.naver.com/main/list.naver', headers=headers, params=params)
     yh = BeautifulSoup(yeonhap.text, 'html.parser')
@@ -25,8 +25,8 @@ for date in range(20230801,20230803) :
     for dt in yh.select('div.list_body li dt') :
 
       if dt.attrs == {} :
-          print(dt.find('a').text.strip(), yh.select_one('div.list_body li > dl > dd > span.date').text)  # 기사제목, 날짜
-          # print(dt.find('a').attrs['href'].strip())   # 뉴스링크
+          print(dt.find('a').text.strip(), yh.select_one('div.list_body li > dl > dd > span.date').text.split(' ')[0])  # 기사제목, 날짜
+          print(dt.find('a').attrs['href'].strip())   # 뉴스링크
           result = re.search('[0-9]{10}', dt.find('a').attrs['href'])
           aid = result.group()
           yeonhap2 = requests.get(dt.find('a').attrs['href'].strip())
@@ -34,7 +34,23 @@ for date in range(20230801,20230803) :
 
 
           if yh2.find('article') != None :   # 일반뉴스
-            print(yh2.find('article').text.replace('\n', ' ').strip(),'\n')
+            print(yh2.find('article').text.replace('\n', ' ').strip())
+
+            if yh2.find('div', attrs={'data-ccounttype':'period'}) != None :
+              cid = yh2.find('div', attrs={'data-ccounttype':'period'}).attrs['data-cid']
+              params = {
+                'q': 'JOURNALIST[' + cid + '(period)]|NEWS[ne_001_' + aid + ']'
+                }
+              res = requests.get('https://news.like.naver.com/v1/search/contents', headers=headers, params=params)
+              yhdic = get_react(res)
+              count_react(yhdic['contents'][1]['reactions'])
+
+            else :
+              params = {'q': 'NEWS[ne_001_' + aid + ']'}
+              res = requests.get('https://news.like.naver.com/v1/search/contents', headers=headers, params=params)
+              yhdic = get_react(res)
+              count_react(yhdic['contents'][0]['reactions'])
+
 
 
           elif yh2.find('div', id='newsEndContents') != None :   # 스포츠
@@ -42,12 +58,12 @@ for date in range(20230801,20230803) :
 
             cid = yh2.find('div', attrs={'data-ccounttype':'period'}).attrs['data-cid']
             params = {
-            'q': 'SPORTS[ne_001_' + aid + ']|JOURNALIST[' + cid + '(period)]|SPORTS_MAIN[ne_001_' + aid + ']'
-            }
+              'q': 'SPORTS[ne_001_' + aid + ']|JOURNALIST[' + cid + '(period)]|SPORTS_MAIN[ne_001_' + aid + ']'
+              }
             res = requests.get('https://sports.like.naver.com/v1/search/contents', headers=headers, params=params)
             yhdic = get_react(res)
 
-            print('반응수 :', count_react(yhdic['contents'][0]['reactions']),'\n')
+            count_react(yhdic['contents'][0]['reactions'])
 
 
           elif yh2.find('div', id='articeBody') != None :   # 연예
@@ -56,9 +72,9 @@ for date in range(20230801,20230803) :
 
             cid = yh2.find('div', attrs={'data-ccounttype':'period'}).attrs['data-cid']
             params = {
-              'q': 'ENTERTAIN[ne_001_' + aid + ']|JOURNALIST[' + cid + '(period)]|ENTERTAIN_MAIN[ne_001_' + aid + ']'
-            }
+                'q': 'ENTERTAIN[ne_001_' + aid + ']|JOURNALIST[' + cid + '(period)]|ENTERTAIN_MAIN[ne_001_' + aid + ']'
+              }
             res = requests.get('https://news.like.naver.com/v1/search/contents', headers=headers, params=params)
             yhdic = get_react(res)
 
-            print('반응수 :', count_react(yhdic['contents'][0]['reactions']),'\n')
+            count_react(yhdic['contents'][0]['reactions'])
