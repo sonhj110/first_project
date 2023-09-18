e425fc30eb31a36d58f97e11b66fd25e23ba9ba2
hyojung <sonhj10318@gmail.com>
2023-09-18 20:01:10 +0900

    크롤링 1차 완성

--- yeonhapnews.py
+++ yeonhapnews.py
@@ -6,8 +6,8 @@ import json
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'}
 
 def modify(body) :
-  body = BeautifulSoup(re.sub('<em class="img_desc">((?!<\/em>).)*<\/em>', '', str(body))).text
-  result = re.search('.*(?=[^a-zA-Z0-9][a-zA-Z0-9]+@[a-zA-Z]+\.(com|kr|co.kr))', body)
+  # body = BeautifulSoup(re.sub('<em class="img_desc">((?!<\/em>).)*<\/em>', '', str(body))).text
+  result = re.search('(.|\n)*(?=[^a-zA-Z0-9][a-zA-Z0-9]+@[a-zA-Z]+\.(com|kr|co.kr))', body.text)
   body = result.group()
   body = re.sub('\([가-힣]+\=연합뉴스\) [가-힣]{2,4} (기자|특파원|통신원) \=', '', body) 
   return body.replace('\n', ' ').strip()
@@ -26,7 +26,7 @@ def get_react(link) :
 
 for date in range(20230801,20230803) :
 
-  for page in range(3,6) :
+  for page in range(1,3) :
     params = {'mode':'LPOD', 'mid':'sec', 'oid':'001', 'date':date, 'page':page}
     yeonhap = requests.get('https://news.naver.com/main/list.naver', headers=headers, params=params)
     yh = BeautifulSoup(yeonhap.text, 'html.parser')
@@ -44,6 +44,7 @@ for date in range(20230801,20230803) :
 
           if yh2.find('article') != None :   # 일반뉴스
             # print(yh2.find('article').text.replace('\n', ' ').strip())
+            print(yh2.select_one('em.media_end_categorize_item').text,'섹션')
             print(modify(yh2.find('article')))
 
             if yh2.find('div', attrs={'data-ccounttype':'period'}) != None :
@@ -64,8 +65,10 @@ for date in range(20230801,20230803) :
 
 
           elif yh2.find('div', id='newsEndContents') != None :   # 스포츠
+            print('스포츠 섹션')
             # print(yh2.find('div', id='newsEndContents').text.replace('\n', ' ').strip())
             print(modify(yh2.find('div', id='newsEndContents')))
+            print('스포츠 섹션')
 
             cid = yh2.find('div', attrs={'data-ccounttype':'period'}).attrs['data-cid']
             params = {
@@ -78,6 +81,7 @@ for date in range(20230801,20230803) :
 
 
           elif yh2.find('div', id='articeBody') != None :   # 연예
+            print('연예 섹션')
             # print(yh2.find('div', id='articeBody').text.replace('\n', ' ').strip())
             print(modify(yh2.find('div', id='articeBody')))
 
