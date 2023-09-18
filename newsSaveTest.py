46171d8aee4583ea330059fd26c0c994f2c695e9
hyojung <sonhj10318@gmail.com>
2023-09-18 21:06:50 +0900

    csv로 저장 아직 시도중

--- newsSaveTest.py
+++ newsSaveTest.py
@@ -2,15 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 import re 
 import json
+import csv
+file = open("news.csv",mode="w",encoding="utf-8",newline="")
+writer = csv.writer(file)
+
+writer.writerow(['제목','날짜','분류','본문','반응수'])
 
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'}
 
 def modify(body) :
   # body = BeautifulSoup(re.sub('<em class="img_desc">((?!<\/em>).)*<\/em>', '', str(body))).text
   try :
-    result = re.search('(.|\n)*(?=[^a-zA-Z0-9][a-zA-Z0-9]+@[a-zA-Z]+\.(com|kr|co.kr))', body)
-    body = result.group()
-  except Exception as e :
+    del_email = re.search('(.|\n)*(?=[^a-zA-Z0-9][a-zA-Z0-9]+@[a-zA-Z]+\.(com|kr|co.kr))', body)
+    body = del_email.group()
+  except :
     print('', end='')
   finally :
     body = re.sub('\([가-힣]+\=연합뉴스\)', '', body)
@@ -21,15 +26,15 @@ def count_react(dic) :
   reactNum = 0
   for emo in dic :
     reactNum = reactNum + int(emo['count'])
-  return print('반응수 :', reactNum, '\n')
+  return reactNum
 
 def get_react(link) :
-  result = re.search('{.+}', link.text)
-  return json.loads(result.group())
+  reactLink = re.search('{.+}', link.text)
+  return json.loads(reactLink.group())
 
 
 
-for date in range(20230813,20230815) :
+for date in range(20230805,20230807) :
 
   for page in range(50,52) :
     params = {'mode':'LPOD', 'mid':'sec', 'oid':'001', 'date':date, 'page':page}
@@ -39,8 +44,10 @@ for date in range(20230813,20230815) :
     for dt in yh.select('div.list_body li dt') :
 
       if dt.attrs == {} :
-          print(dt.find('a').text.strip(), yh.select_one('div.list_body li > dl > dd > span.date').text.split(' ')[0])  # 기사제목, 날짜
-          print(dt.find('a').attrs['href'].strip())   # 뉴스링크
+          temp = []
+          temp.append(dt.find('a').text.strip())
+          temp.append(date)
+
           result = re.search('[0-9]{10}', dt.find('a').attrs['href'])
           aid = result.group()
           yeonhap2 = requests.get(dt.find('a').attrs['href'].strip())
@@ -48,9 +55,9 @@ for date in range(20230813,20230815) :
 
 
           if yh2.find('article') != None :   # 일반뉴스
-            # print(yh2.find('article').text.replace('\n', ' ').strip())
-            print(yh2.select_one('em.media_end_categorize_item').text,'섹션')
-            print(modify(yh2.find('article').text))
+
+            temp.append(yh2.select_one('em.media_end_categorize_item').text)
+            temp.append(modify(yh2.find('article').text))
 
             if yh2.find('div', attrs={'data-ccounttype':'period'}) != None :
               cid = yh2.find('div', attrs={'data-ccounttype':'period'}).attrs['data-cid']
@@ -59,21 +66,19 @@ for date in range(20230813,20230815) :
                 }
               res = requests.get('https://news.like.naver.com/v1/search/contents', headers=headers, params=params)
               yhdic = get_react(res)
-              count_react(yhdic['contents'][1]['reactions'])
+              temp.append(count_react(yhdic['contents'][1]['reactions']))
 
             else :
               params = {'q': 'NEWS[ne_001_' + aid + ']'}
               res = requests.get('https://news.like.naver.com/v1/search/contents', headers=headers, params=params)
               yhdic = get_react(res)
-              count_react(yhdic['contents'][0]['reactions'])
+              temp.append(count_react(yhdic['contents'][0]['reactions']))
 
 
 
           elif yh2.find('div', id='newsEndContents') != None :   # 스포츠
-            print('스포츠 섹션')
-            # print(yh2.find('div', id='newsEndContents').text.replace('\n', ' ').strip())
-            print(modify(yh2.find('div', id='newsEndContents').text))
-            print('스포츠 섹션')
+            temp.append('스포츠')
+            temp.append(modify(yh2.find('div', id='newsEndContents').text))
 
             cid = yh2.find('div', attrs={'data-ccounttype':'period'}).attrs['data-cid']
             params = {
@@ -81,13 +86,12 @@ for date in range(20230813,20230815) :
               }
             res = requests.get('https://sports.like.naver.com/v1/search/contents', headers=headers, params=params)
             yhdic = get_react(res)
-            count_react(yhdic['contents'][0]['reactions'])
+            temp.append(count_react(yhdic['contents'][0]['reactions']))
 
 
           elif yh2.find('div', id='articeBody') != None :   # 연예
-            print('연예 섹션')
-            # print(yh2.find('div', id='articeBody').text.replace('\n', ' ').strip())
-            print(modify(yh2.find('div', id='articeBody').text))
+            temp.append('연예')
+            temp.append(modify(yh2.find('div', id='articeBody').text))
 
             cid = yh2.find('div', attrs={'data-ccounttype':'period'}).attrs['data-cid']
             params = {
@@ -95,4 +99,10 @@ for date in range(20230813,20230815) :
               }
             res = requests.get('https://news.like.naver.com/v1/search/contents', headers=headers, params=params)
             yhdic = get_react(res)
-            count_react(yhdic['contents'][0]['reactions'])
+            temp.append(count_react(yhdic['contents'][0]['reactions']))
+
+          print(temp)
+          writer.writerow(temp)
+      
+
+file.close()
