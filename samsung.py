bff5877ef7495eb856dd34be6d4ad15f6e3bf9c4
hyojung <sonhj10318@gmail.com>
2023-09-19 17:45:36 +0900

    삼성 기사만 뽑기

---
+++ samsung.py
@@ -0,0 +1,16 @@
+import csv
+
+new_file = open('samsung.csv', 'w', encoding='utf-8')
+writer = csv.writer(new_file)
+
+with open('news.csv', 'r', encoding='utf-8') as file :
+    rdr = csv.reader(file)
+
+    for data in rdr :
+        if data[4].find('삼성전자') != -1 :
+            writer.writerow(data)
+
+new_file.close()
+
+
+
